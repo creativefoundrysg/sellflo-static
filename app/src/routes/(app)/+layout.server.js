412c19ed2272ce3e9f3b5832e6e3aabf2aed7bdb
Neya <dsignr@users.noreply.github.com>
2025-07-14 20:30:59 +0800

    Set CF Prospect ID

--- app/src/routes/(app)/+layout.server.js
+++ app/src/routes/(app)/+layout.server.js
@@ -1,11 +1,24 @@
+// @ts-nocheck
+// import crypto from 'crypto';
 import {randomBytes} from 'node:crypto';
 
-export async function load({ cookies }) {
+/** @type {import('./$types').LayoutServerLoad} */
+export async function load({ cookies, url }) {
 	// @ts-ignore
     const csrfToken = randomBytes(32).toString('hex');
     cookies.set('csrfToken', csrfToken, { httpOnly: true, sameSite: 'strict', path: '/' });
 
+	const t = url.searchParams.get('t');
+
+	if (t) {
+		cookies.set('cf_prospect_uid', t, {
+			httpOnly: true,
+			sameSite: 'strict',
+			path: '/'
+		});
+	}
+
 	return {
-		csrfToken: csrfToken
+		csrfToken: csrfToken,
 	}
 }
