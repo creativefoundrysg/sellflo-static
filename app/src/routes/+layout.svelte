e9f7a751879beec3b57039dcc735f8cb2f350136
Neya <dsignr@users.noreply.github.com>
2025-09-16 00:24:42 +0800

    Conditionally render header

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -1,10 +1,17 @@
 <script>
     import "../app.css"
+    import Header from "$components/header/standard.svelte";
     import Footer from "$components/footer/standard.svelte";
+    import { page } from '$app/stores';
 
 	let { children } = $props();
     // #f3124e
 </script>
+
+<!-- Don't show on homepage -->
+{#if $page.url.pathname !== '/'}
+  <Header />
+{/if}
 
 {@render children()}
 
